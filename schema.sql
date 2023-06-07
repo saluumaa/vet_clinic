379aee58013fe9211884b763f86fd05f8652a82d
saluumaa <salumahassan02@gmail.com>
2023-06-07 16:13:25 +0300

    Create table owners

--- schema.sql
+++ schema.sql
@@ -8,3 +8,17 @@ escape_attempts INT,
 neutered BOOLEAN,
 weight_kg FLOAT 
 );
+
+/* Alter the database schema to add a new column. */
+ALTER TABLE animals ADD species varchar(255);
+
+CREATE TABLE owners(
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+full_name VARCHAR(250),
+age INT
+);
+
+
+
+
+
