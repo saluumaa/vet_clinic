086d999daac2105487998799f790747d373c409c
saluumaa <salumahassan02@gmail.com>
2023-06-07 19:45:04 +0300

    Create tables and add relationships

--- schema.sql
+++ schema.sql
@@ -38,3 +38,29 @@ ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD CONSTRAINT FK_species FOREIGN KEY(species_id) REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT FK_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+-------- relationship between tables ------------
+
+CREATE TABLE vets (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(100),
+  age INT,
+  date_of_graduation DATE
+);
+
+--- create a table to store the relationship between vets and species
+
+CREATE TABLE specializations (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  vet_id INT REFERENCES vets(id),
+  species_id INT REFERENCES species(id)
+);
+
+--- create a table to store the relationship between vets and animals
+
+CREATE TABLE visits(
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+vet_id INT REFERENCES vets(id),
+animals_id INT REFERENCES animals(id),
+visit_date DATE
+);
