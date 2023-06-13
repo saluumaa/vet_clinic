1412ef5343f50a8f5ae9992a97d0ecd1a18ae5b4
TayyabKM <109720981+TayyabKM@users.noreply.github.com>
2023-06-13 18:28:01 +0200

    Add new data

--- schema.sql
+++ schema.sql
@@ -66,3 +66,9 @@ visit_date DATE
 
 
 CREATE INDEX ON visits (animal_id);
+
+---new data
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
