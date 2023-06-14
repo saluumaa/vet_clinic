2c6787fa13284350664116856ce9e143f28e83d6
saluumaa <salumahassan02@gmail.com>
2023-06-14 12:05:15 +0300

    Create database based on the diagram

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,35 @@
+CREATE TABLE patients (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(100),
+  date_of_birth DATE,
+);
+
+CREATE TABLE medical_history (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  admitted_at timestamp,
+  patient_id INT REFERENCES patients(id),
+  status VARCHAR(100),
+);
+
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  total_amount FLOAT,
+  generated_at timestamp,
+  payed_at timestamp,
+  medical_history_id INT REFERENCES medical_history(id),
+);
+
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  unit_price FLOAT,
+  quantity INT,
+  total_price FLOAT,
+  invoice_id INT REFERENCES invoices(id),
+  treatment_id INT REFERENCES treatments(id),
+);
+
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  type VARCHAR(100),
+  name VARCHAR(100),
+);
